6dbc0c1b1a80e700347677b1649d2c1b0c9b7e82
easyiom <100007563.joan23@fje.edu>
2021-11-23 15:49:27 +0100

    estructura html inicial

--- view/inicio.php
+++ view/inicio.php
@@ -1,3 +1,27 @@
 <?php
-    echo "Hola";
+
+require_once '../services/conection.php';
+
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Events</title>
+    <!-- librerias-->
+    <script type="text/javascript" src="../js/jquery.js"></script><!-- jquery-->
+    <!-- <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script> -->
+    <script src="https://cdn.jsdelivr.net/npm/js-cookie@2.2.0/src/js.cookie.min.js"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script><!-- sweetalert-->
+    <script type="text/javascript" src="../js/iconos_g.js"></script><!-- iconos FontAwesome-->
+    <script type="text/javascript" src="../js/js.js"></script>
+    <link rel="icon" type="image/png" href="../img/icon.png">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body class="menu">
+</body>
+</html>
